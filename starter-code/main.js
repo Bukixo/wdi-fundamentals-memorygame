515dd2354b7caaad59acbe641d2167c698e2ce0f
Buki <bukithompson@hotmail.co.uk>
2017-01-10 15:04:01 +0000

    added alerts for logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,18 @@
 console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+
+if ( cardOne === cardTwo) || (cardFour === cardThree) {
+
+	alert('You found a match!');
+}
+ else if (cardOne === cardThree) || (cardOne === cardFour) {
+ 	alert("Sorry, try again.");
+ }
+
+ else ( cardTwo === cardThree) || (cardTwo === cardFour) {
+ 	alert("Sorry, try again.");
+ }
